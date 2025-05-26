405c55feaf3b3bdaa38b193406e5b9334eaf95d6
emaag <eric.maag@merkle.com>
2025-05-26 11:28:33 +0100

    Created routes, initial testing

--- app/Http/Controllers/SpotifyController.php
+++ app/Http/Controllers/SpotifyController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class SpotifyController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'artist' => 'White Stripes',
+            'album' => 'Elephant',
+            'track' => 'Seven Nation Army'
+        ]);
+    }
 }
